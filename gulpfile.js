1422afc2fc2b1cfbfc43f8ff23415eedbcc1a116
Victor Gomes <victorgsilva92@gmail.com>
2020-02-28 16:23:01 -0300

    gulpfile.js configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,9 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 clean = require('gulp-clean'),
-uglify = require('gulp-uglify'),
-usemin = require('gulp-usemin'),
 browserSync = require('browser-sync').create(),
-autoprefixer = require('gulp-autoprefixer'),
+gulpSequence = require ('gulp-sequence'),
 sass = require('gulp-sass');
-
-gulp.task('default', ['copy'], function () {
-    gulp.start('build-img', 'usemin');
-});
 
 gulp.task('copy', ['clean'], function () {
     return gulp.src('src/**/*')
@@ -28,28 +22,22 @@ gulp.task('build-img', function () {
     .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('usemin', function () {
-    return gulp.src('dist/**/*.html')
-    .pipe(usemin({
-        js: [uglify],
-        css: [autoprefixer]
-    }))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('server', function () {
+gulp.task('serve', function () {
     browserSync.init({
         server: {
             baseDir: 'src'
         }
     });
-    gulp.watch('src/sass/**/*scss',['sass']);
+    gulp.watch('src/sass/**/*.scss',['sass']).on('change', browserSync.reload);
     gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
 });
 
 gulp.task('sass',function(){
-    return gulp.src('src/sass/estilos.scss')
+    return gulp.src('src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
+
+gulp.task("default", gulpSequence('copy', 'build-img', 'sass', 'serve'));
