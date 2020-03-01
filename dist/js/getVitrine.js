56229b262a67975db00db6dea12a6639a09af3bf
Victor Gomes <victorgsilva92@gmail.com>
2020-03-01 19:17:54 -0300

    add section banners

--- dist/js/getVitrine.js
+++ dist/js/getVitrine.js
@@ -1,22 +1,36 @@
-// $(document).ready(function () {
-//     $.getJSON('../moto.json', function(data) {
+$(document).ready(function () {
+    $.getJSON('../moto.json', function(data) {
         
-//         var produtos = '';
-//         $.each(data, function(index, element) {
-//             console.log(element)
+        var produtos = '';
+        $.each(data, function(index, element) {
+            console.log(element)
             
-//             produtos += '<div class="produtos">'
-//             + '<div class="id">'+element[1].name+'</div>'
-//             + '<div class="nome">'
-//             + '<img src="'+element[1].image + '"' +'/>'
-//             + '</div>' 
-//             + '<div class="imagem">'+element[1].price+'</div>'
-//             + '</div>';
-//         });
+            var count = Object.keys(element).length;
+            console.log(count);
+            
+            var i;
+            
+            for (i = 1; i < count+1; i++) {
+                produtos += 
+                '<div class="produtos-card">'
+                    + '<div class="produtos-desconto">'
+                        + '<span>-30%</span>'
+                    + '</div>'
 
+                    + '<img src="'+element[i].image + '"' +'/>'
+                    
+                    + '<div class="produtos-text">'
+                        + '<p class="produtos-nome">'+element[i].name+'</p>'
+                        + '<p class="produtos-valorAntigo">De: R$ 2.000,00</p>'
+                        + '<p class="produtos-preco">Por: R$'+element[i].price+' em até <b>12X</b> de <b>R$ 149,91</b></p>'
+                    + '</div>'
+                    + '<button class="produtos-btnComprar">COMPRAR</button>'
+                + '</div>';
 
+                $("#vitrineProdutos").html(produtos);
+            }
+        });
         
-//         $("#vitrineProdutos").html(produtos);
         
-//     });
-// })
+    });
+})
